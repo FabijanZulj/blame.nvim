33471456cf18b073a7174768ff19e526f772d192
FabijanZulj <fabijan.zulj@gmail.com>
2024-04-12 23:45:50 +0200

    rewrite: allow custom views, new feat blame stack, quick commit info etc.

--- lua/blame.lua
+++ lua/blame.lua
@@ -1,125 +1,99 @@
-local virtual_blame = require("blame.virtual_blame")
-local window_blame = require("blame.window_blame")
-local blame_parser = require("blame.blame_parser")
-local highlights = require("blame.highlights")
 local git = require("blame.git")
+local window_view = require("blame.views.window_view")
+local porcelain_parser = require("blame.porcelain_parser")
+local virtual_view = require("blame.views.virtual_view")
+local formats = require("blame.formats.default_formats")
+
+---@class BlameView
+---@field new fun(self, config:Config) : BlameView
+---@field open fun(self, lines: string[])
+---@field is_open fun(): boolean
+---@field close fun(cleanup: boolean)
+
+---@alias FormatFn fun(line_porcelain: Porcelain, config:Config, idx:integer):LineWithHl
+
+---@class Mappings
+---@field commit_info string | string[]
+---@field stack_push string | string[]
+---@field stack_pop string | string[]
+---@field show_commit string | string[]
+---@field close string | string[]
 
 ---@class Config
----@field date_format string Format of the output date
----@field format function|nil Custom blame formmatter, takes a table containing key-values pair of git blame --line-porcelaine (and an aditional date value)
----@field width number|nil Manual setup of window width
----@field virtual_style "float"|"right_align"
----@field merge_consecutive boolean Should same commits be ignored after first line
----@field commit_detail_view string "tab"|"split"|"vsplit"|"current" How to open commit details
----@field colors string[]|nil Custom colors for the highlights
+---@field date_format? string Format of the output date
+---@field views table<string, BlameView>
+---@field merge_consecutive boolean Merge consecutive commits and don't repeat
+---@field virtual_style 'float' | any Style of the virtual view
+---@field format_fn FormatFn Function that formats the output, default: require("blame.formats.default_formats").date_message
+---@field max_summary_width number Max width of the summary in 'date_summary' format
+---@field show_view_type 'current' | 'tab' | 'vsplit' | 'split'
+---@field mappings Mappings
 local config = {
-	date_format = "%Y/%m/%d %H:%M",
-	format = nil,
-	width = nil,
-	virtual_style = "right_align",
-	merge_consecutive = false,
-	commit_detail_view = "tab",
-    colors = nil,
+    date_format = "%d.%m.%Y",
+    views = {
+        window = window_view,
+        virtual = virtual_view,
+        default = window_view,
+    },
+    merge_consecutive = false,
+    max_summary_width = 30,
+    format_fn = formats.date_message,
+    show_view_type = "vsplit",
+    mappings = {
+        commit_info = "i",
+        stack_push = "<TAB>",
+        stack_pop = "<BS>",
+        show_commit = "<CR>",
+        close = { "<esc>", "q" },
+    },
 }
 
----@class Blame
----@field config Config
----@field blame_lines table[]
+---@param blame_view BlameView
+local function open(blame_view)
+    local filename = vim.api.nvim_buf_get_name(0)
+    local cwd = vim.fn.expand("%:p:h")
+    local g = git:new(config)
+    g:blame(filename, cwd, nil, function(data)
+        vim.schedule(function()
+            local parsed_blames = porcelain_parser.parse_porcelain(data)
+            blame_view:open(parsed_blames)
+        end)
+    end)
+end
+
 local M = {}
 
----@type Config
-M.config = config
----@type string[]
-M.blame_lines = {}
-M.blame_nvim_augroup = vim.api.nvim_create_augroup("BlameNvim", { clear = false })
+---@param setup_args Config | nil
+M.setup = function(setup_args)
+    config = vim.tbl_deep_extend("force", config, setup_args or {})
 
----@param args Config?
-M.setup = function(args)
-	M.config = vim.tbl_deep_extend("force", M.config, args or {})
-	highlights.setup_highlights()
-	vim.api.nvim_create_autocmd('ColorScheme', {
-		-- group = 'blame_nvim',
-		callback = highlights.setup_highlights,
-	})
-end
+    local blame_view = config.views.default:new(config)
+    local last_opened_view = nil
 
----Creates a done hook for the 'git blame' command depending on the blame_type to show
----@param blame_type "window"|"virtual"|""
----@return function
-local function done(blame_type)
-	return function(_, exit_code)
-		if exit_code ~= 0 then
-			vim.notify("Could not execute blame, might not be a git repository", vim.log.levels.INFO)
-			return
-		end
-		local parsed_blames = blame_parser.parse_porcelain(M.blame_lines)
-		highlights.map_highlights_per_hash(parsed_blames, M.config)
+    vim.api.nvim_create_user_command("BlameToggle", function(args)
+        config.ns_id = vim.api.nvim_create_namespace("blame_ns")
 
-		if blame_type == "window" or blame_type == "" then
-			window_blame.window_blame(parsed_blames, M.config)
-		elseif blame_type == "virtual" then
-			virtual_blame.virtual_blame(parsed_blames, M.config)
-		end
-	end
-end
-
----Captures the raw porcelain formatted data returned from git blame
----@param _ any
----@param data string[]
-local function stdout(_, data)
-	M.blame_lines = data
-end
-
----@param blame_type "window"|"virtual"|""
-local function toggle(blame_type)
-	local is_window_open = window_blame.is_window_open()
-	local is_virtual_open = virtual_blame.nsId ~= nil
-
-	if is_window_open or is_virtual_open then
-		if is_window_open then
-			window_blame.close_window()
-		end
-		if is_virtual_open then
-			virtual_blame.close_virtual()
-		end
-		return
-	end
-	git.blame(vim.api.nvim_buf_get_name(0), vim.fn.expand('%:p:h'), done(blame_type), stdout)
-end
-
----@param blame_type "window"|"virtual"|""
-local function enable(blame_type)
-	local is_window_open = window_blame.is_window_open()
-	local is_virtual_open = virtual_blame.nsId ~= nil
-
-	if is_window_open or is_virtual_open then
-		return
-	else
-		git.blame(vim.api.nvim_buf_get_name(0), vim.fn.expand('%:p:h'), done(blame_type), stdout)
-	end
-end
-
-local function disable()
-	local is_window_open = window_blame.is_window_open()
-	local is_virtual_open = virtual_blame.nsId ~= nil
-
-	if is_window_open then
-		window_blame.close_window()
-	elseif is_virtual_open then
-		virtual_blame.close_virtual()
-	end
-end
-
-M.toggle = function(arguments)
-	return toggle(arguments["args"])
-end
-
-M.enable = function(arguments)
-	return enable(arguments["args"])
-end
-
-M.disable = function()
-	return disable()
+        if last_opened_view ~= nil and last_opened_view:is_open() then
+            last_opened_view:close(false)
+            last_opened_view = nil
+        else
+            local arg = args.args == "" and "default" or args.args
+            blame_view = config.views[arg]:new(config)
+            open(blame_view)
+            last_opened_view = blame_view
+        end
+    end, {
+        nargs = "?",
+        complete = function()
+            local all_views = {}
+            for k, _ in pairs(config.views) do
+                if k ~= "default" then
+                    table.insert(all_views, k)
+                end
+            end
+            return all_views
+        end,
+    })
 end
 
 return M
---
+++ lua/blame/blame_stack.lua
@@ -0,0 +1,249 @@
+local git = require("blame.git")
+local utils = require("blame.utils")
+local porcelain_parser = require("blame.porcelain_parser")
+local mappings = require("blame.mappings")
+---@class BlameStack
+---@field config Config
+---@field blame_view BlameView
+---@field git_client Git
+---@field stack_buffer integer
+---@field stack_info_float_win integer
+---@field original_window integer
+---@field original_buffer integer
+---@field file_path string
+---@field cwd string
+---@field commit_stack Porcelain[]
+local BlameStack = {}
+
+---@return BlameStack
+function BlameStack:new(config, blame_view, original_window, file_path, cwd)
+    local o = {}
+    setmetatable(o, { __index = self })
+
+    o.config = config
+    o.blame_view = blame_view
+    o.original_window = original_window
+    o.original_buffer = vim.api.nvim_win_get_buf(o.original_window)
+    o.file_path = file_path
+    o.cwd = cwd
+
+    o.stack_buffer = nil
+    o.git_client = git:new(config)
+    o.commit_stack = {}
+
+    return o
+end
+
+function BlameStack:pop()
+    if #self.commit_stack == 0 then
+        return
+    end
+    if #self.commit_stack == 1 then
+        self:reset_to_original_buf()
+        return
+    end
+    table.remove(self.commit_stack, nil)
+    self:open_stack_info_float()
+    self:show_file_content(
+        self.commit_stack[#self.commit_stack],
+        self.stack_buffer,
+        function()
+            vim.api.nvim_set_current_win(self.original_window)
+            self:blame_for_commit(self.commit_stack[#self.commit_stack])
+        end
+    )
+end
+
+function BlameStack:reset_to_original_buf()
+    self.commit_stack = {}
+    vim.api.nvim_set_current_win(self.original_window)
+    vim.api.nvim_set_current_buf(self.original_buffer)
+    if self.stack_buffer and vim.api.nvim_buf_is_valid(self.stack_buffer) then
+        vim.api.nvim_buf_delete(self.stack_buffer, { force = true })
+    end
+    self:blame_for_commit({ hash = nil })
+    self.stack_buffer = nil
+    self:close_stack_info_float()
+end
+
+function BlameStack:open_stack_info_float()
+    if self.stack_info_float_win ~= nil then
+        local info_buf = vim.api.nvim_win_get_buf(self.stack_info_float_win)
+        local lines_text = {}
+        for _, v in pairs(self.commit_stack) do
+            table.insert(
+                lines_text,
+                string.sub(v.hash, 0, 7)
+                    .. " "
+                    .. v.author
+                    .. " "
+                    .. os.date(self.config.date_format, v.committer_time)
+            )
+        end
+        vim.api.nvim_buf_set_lines(info_buf, 0, -1, false, lines_text)
+
+        local lns = vim.api.nvim_buf_get_lines(info_buf, 0, -1, false)
+        for idx, v in ipairs(lns) do
+            vim.api.nvim_buf_add_highlight(
+                info_buf,
+                -1,
+                idx == #lns and string.sub(v, 0, 7) or "Comment",
+                idx - 1,
+                0,
+                -1
+            )
+        end
+
+        local width = utils.longest_string_in_array(lines_text) + 5
+        local height = #self.commit_stack
+        vim.api.nvim_win_set_height(self.stack_info_float_win, height)
+        vim.api.nvim_win_set_width(self.stack_info_float_win, width)
+        vim.api.nvim_win_set_cursor(self.stack_info_float_win, { height, 0 })
+        return
+    end
+
+    local info_buf = vim.api.nvim_create_buf(false, true)
+    local lines_text = {}
+    for _, v in pairs(self.commit_stack) do
+        table.insert(
+            lines_text,
+            string.sub(v.hash, 0, 7)
+                .. " "
+                .. v.author
+                .. " "
+                .. os.date(self.config.date_format, v.committer_time)
+        )
+    end
+    vim.api.nvim_buf_set_lines(info_buf, 0, -1, false, lines_text)
+
+    vim.api.nvim_buf_add_highlight(
+        info_buf,
+        -1,
+        string.sub(lines_text[1], 0, 7),
+        0,
+        0,
+        -1
+    )
+
+    local width = utils.longest_string_in_array(lines_text) + 5
+    local height = #self.commit_stack
+
+    self.stack_info_float_win = vim.api.nvim_open_win(info_buf, false, {
+        relative = "win",
+        col = vim.api.nvim_win_get_width(self.original_window),
+        win = self.original_window,
+        row = 1,
+        width = width,
+        height = height,
+        border = "rounded",
+    })
+    vim.wo[self.stack_info_float_win].number = false
+    vim.wo[self.stack_info_float_win].relativenumber = false
+    vim.wo[self.stack_info_float_win].signcolumn = "no"
+    vim.wo[self.stack_info_float_win].scrollbind = false
+    vim.wo[self.stack_info_float_win].cursorbind = false
+
+    vim.api.nvim_win_set_height(self.stack_info_float_win, height)
+    vim.api.nvim_win_set_cursor(self.stack_info_float_win, { height, 0 })
+end
+
+function BlameStack:close_stack_info_float()
+    if
+        self.stack_info_float_win
+        and vim.api.nvim_win_is_valid(self.stack_info_float_win)
+    then
+        vim.api.nvim_win_close(self.stack_info_float_win, true)
+    end
+    self.stack_info_float_win = nil
+end
+
+function BlameStack:push(commit)
+    if
+        #self.commit_stack > 0
+        and self.commit_stack[#self.commit_stack].hash == commit.hash
+    then
+        return
+    end
+    if self.stack_buffer == nil then
+        self.stack_buffer = vim.api.nvim_create_buf(true, true)
+        vim.bo[self.stack_buffer].ft = vim.bo[self.original_buffer].ft
+        mappings.set_keymap("n", "stack_pop", function()
+            self:pop()
+        end, {
+            buffer = self.stack_buffer,
+            nowait = true,
+            silent = true,
+            noremap = true,
+        }, self.config)
+
+        vim.api.nvim_buf_set_name(self.stack_buffer, "Blame stack")
+
+        vim.api.nvim_create_autocmd({ "BufHidden", "BufUnload" }, {
+            callback = function()
+                vim.schedule(function()
+                    self:reset_to_original_buf()
+                    self:close()
+                end)
+            end,
+            buffer = self.stack_buffer,
+            group = vim.api.nvim_create_augroup("NvimBlame", { clear = false }),
+            desc = "Reset state when closing blame buffer",
+        })
+    end
+
+    vim.api.nvim_set_current_win(self.original_window)
+    vim.api.nvim_set_current_buf(self.stack_buffer)
+
+    table.insert(self.commit_stack, commit)
+    self:open_stack_info_float()
+
+    self:show_file_content(commit, self.stack_buffer, function()
+        self:blame_for_commit(commit)
+    end)
+end
+
+function BlameStack:close()
+    self:close_stack_info_float()
+    vim.api.nvim_win_set_buf(self.original_window, self.original_buffer)
+    if
+        self.stack_buffer ~= nil
+        and vim.api.nvim_buf_is_valid(self.stack_buffer)
+    then
+        vim.api.nvim_buf_delete(self.stack_buffer, { force = true })
+    end
+    self.stack_buffer = nil
+    self.commit_stack = {}
+end
+
+function BlameStack:blame_for_commit(commit, cb)
+    self.git_client:blame(self.file_path, self.cwd, commit.hash, function(data)
+        vim.schedule(function()
+            local parsed_blames = porcelain_parser.parse_porcelain(data)
+            self.blame_view:open(parsed_blames)
+            if cb ~= nil then
+                cb()
+            end
+        end)
+    end)
+end
+
+function BlameStack:show_file_content(commit, buf, cb)
+    self.git_client:show(
+        self.file_path,
+        self.cwd,
+        commit.hash,
+        function(file_content)
+            vim.schedule(function()
+                -- have to remove laste element as it is a newline always inserted from git show
+                table.remove(file_content)
+
+                vim.api.nvim_buf_set_lines(buf, 0, -1, false, file_content)
+                if cb ~= nil then
+                    cb()
+                end
+            end)
+        end
+    )
+end
+
+return BlameStack

bcfbc4cedb47b1af0e8d1cd0ef81f44851fa2b7b
JanosSarkoezi <j.sarkoezi@googlemail.com>
2024-06-02 20:19:13 +0200

    added configurable blame options

--- lua/blame/git.lua
+++ lua/blame/git.lua
@@ -11,65 +11,6 @@ function Git:new(config)
     return o
 end
 
----Execute git blame line porcelain command, returns output string
----@param filename string
----@param cwd any cwd where to execute the command
----@param commit string|nil
----@param callback fun(data: string[]) callback on exiting the command with output string
-function Git:blame(filename, cwd, commit, callback, err_cb)
-    local blame_command = {
-        "git",
-        "--no-pager",
-        "blame",
-        "--line-porcelain",
-        filename,
-    }
-    if commit ~= nil then
-        table.insert(blame_command, 5, commit)
-    end
-    local data
-    local err_data
-    vim.fn.jobstart(blame_command, {
-        cwd = cwd,
-        on_exit = function(_, exit_code)
-            if exit_code ~= 0 then
-                return err_cb(table.concat(err_data, " "))
-            end
-            callback(data)
-        end,
-        on_stderr = function(_, d)
-            err_data = d
-        end,
-        on_stdout = function(_, d)
-            data = d
-        end,
-        stdout_buffered = true,
-        stderr_buffered = true,
-    })
-end
-
-function Git:git_root(cwd, callback, err_cb)
-    local data
-    local err_data
-    vim.fn.jobstart({ "git", "rev-parse", "--show-toplevel" }, {
-        cwd = cwd,
-        on_exit = function(_, exit_code)
-            if exit_code ~= 0 then
-                return err_cb(table.concat(err_data, " "))
-            end
-            callback(data)
-        end,
-        on_stderr = function(_, d)
-            err_data = d
-        end,
-        on_stdout = function(_, d)
-            data = d
-        end,
-        stdout_buffered = true,
-        stderr_buffered = true,
-    })
-end
-
 local function execute_command(command, cwd, callback, err_cb)
     local data
     local err_data
@@ -90,6 +31,42 @@ local function execute_command(command, cwd, callback, err_cb)
         stdout_buffered = true,
         stderr_buffered = true,
     })
+end
+
+local function add_blame_options(blame_command, blame_options)
+    if blame_options == nil then
+        return
+    end
+
+    local index = #blame_command - 2
+    for i = 1, #blame_options do
+        table.insert(blame_command, index + i, blame_options[i])
+    end
+end
+
+---Execute git blame line porcelain command, returns output string
+---@param filename string
+---@param cwd any cwd where to execute the command
+---@param commit string|nil
+---@param callback fun(data: string[]) callback on exiting the command with output string
+function Git:blame(filename, cwd, commit, callback, err_cb)
+    local blame_command = {
+        "git",
+        "--no-pager",
+        "blame",
+        "--line-porcelain",
+        filename,
+    }
+    add_blame_options(blame_command, self.config.blame_options)
+    if commit ~= nil then
+        table.insert(blame_command, #blame_command - 1, commit)
+    end
+    execute_command(blame_command, cwd, callback, err_cb)
+end
+
+function Git:git_root(cwd, callback, err_cb)
+    local rev_parse_command = { "git", "rev-parse", "--show-toplevel" }
+    execute_command(rev_parse_command, cwd, callback, err_cb)
 end
 
 ---Execute git show
